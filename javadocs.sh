b5eda24fd97f809b6ded6dca1d1d9aa7dcf226cc
powercas_gamer <internal@deltapvp.net>
2022-02-08 15:51:14 +0100

    new pyrite version
    Signed-off-by: powercas_gamer <internal@deltapvp.net>

---
+++ javadocs.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# resolve shell-specifics
+case "$(echo "$SHELL" | sed -E 's|/usr(/local)?||g')" in
+    "/bin/zsh")
+        RCPATH="$HOME/.zshrc"
+        SOURCE="${BASH_SOURCE[0]:-${(%):-%N}}"
+    ;;
+    *)
+        RCPATH="$HOME/.bashrc"
+        if [[ -f "$HOME/.bash_aliases" ]]; then
+            RCPATH="$HOME/.bash_aliases"
+        fi
+        SOURCE="${BASH_SOURCE[0]}"
+    ;;
+esac
+
+# get base dir regardless of execution location
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ "$SOURCE" != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SOURCE=$([[ "$SOURCE" = /* ]] && echo "$SOURCE" || echo "$PWD/${SOURCE#./}")
+basedir=$(dirname "$SOURCE")
+COMMAND="$1"
+failed=0
+BRANCH="master"
+case "$COMMAND" in
+    "commit" | "com")
+    (
+        set -e
+        shift
+        cd "$basedir"
+        git add .
+        git commit -m "$@" -s
+        git push origin "$BRANCH"
+    ) || failed=1
+    ;;
+esac
+
+unset RCPATH
+unset SOURCE
+unset basedir
+if [[ "$failed" == "1" ]]; then
+	unset failed
+	false
+else
+	unset failed
+	true
+fi
